1adcedd90931a96a864b0b2e1bac565eaa9a66f4
Bhoraj1 <journeyn556@gmail.com>
2025-06-05 11:51:55 +0545

    completion of banner.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import Banner from "./components/Banner/Banner";
 import Category from "./components/Category/Category";
 import Category2 from "./components/Category/Category2";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Services from "./components/services/Service";
+
+import headphone from './assets/cagegory/headphone.png'
+
+const BannerData = {
+  discount: "30% OFF",
+  title: "Fine Smile",
+  date: "10 Jan to 28 Jan",
+  image: headphone,
+  title2: "Air Solo Bass",
+  title3: "Winter Sale",
+  title4: "Circuit boards, soldering, using a multimeter or oscilloscope.",
+  className:"bg-red-500"
+};
 
 const App = () => {
   return (
@@ -12,6 +26,7 @@ const App = () => {
       <Category />
       <Category2 />
       <Services />
+      <Banner data={BannerData} />
     </div>
   );
 };
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,40 @@
+import Button from "../shared/Button";
+
+const Banner = ({ data }) => {
+  return (
+    <div className="px-[3rem] my-12 ">
+      <div
+        className={` ${data.className} grid grid-cols-1 sm:grid-cols-3 gap-6 items-center text-white rounded-3xl`}
+      >
+        {/* first col */}
+        <div className="p-8">
+          <p className="text-sm">{data.discount}</p>
+          <h1 className="uppercase text-4xl lg:text-7xl font-bold">
+            {data.title2}
+          </h1>
+          <p className="text-sm">{data.date}</p>
+        </div>
+        {/* second col */}
+        <div className="h-full flex justify-center items-center">
+          <img
+            src={data.image}
+            alt="Banner image"
+            className="scale-x-125 w-[250px] md:w-[350px] drop-shadow-2xl object-contain"
+          />
+        </div>
+
+        {/* third col */}
+        <div className="flex flex-col justify-center gap-4 p-6 sm:p-8">
+          <p className="font-bold text-xl">{data.title2}</p>
+          <p className="text-3xl sm:text-5xl font-semibold ">{data.title3}</p>
+          <p className="text-sm tracking-wide leading-5">{data.title4}</p>
+          <div>
+            <Button text="Show Now" className="bg-white text-red-600" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
